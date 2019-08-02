207b1bd09ba76961c36f728d2527d3554c9d2f62
lidia romo <lidiaromogtz94@gmail.com>
2019-08-02 17:40:41 -0500

    define numero de orden correccion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("WorkOrderNo""PO00000089");
+    response.end("WorkOrderNo":"PO00000089");
 });
 
 const port = process.env.PORT || 80;
